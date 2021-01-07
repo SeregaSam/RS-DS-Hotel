1455537e9056bf3b03e992fcf9d32d6bb6cefd61
Maxon <chebola.maxim@gmail.com>
2021-01-07 19:47:43 +0300

    модель room

--- models/room.php
+++ models/room.php
@@ -5,7 +5,5 @@ use yii\db\ActiveRecord;
 
 class room extends  ActiveRecord
 {
-//     public static function tableName() {// если отличается название таблицы
-//        return 'room' ;
-//     }
+
 }
