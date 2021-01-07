3ce9c84eba9f7ad0a95df3ca0c642ea89f3757db
Maxon <chebola.maxim@gmail.com>
2021-01-07 17:14:56 +0300

    Изменены стили AppAssets.php

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -9,6 +9,7 @@ namespace app\assets;
 
 use yii\web\AssetBundle;
 
+
 /**
  * Main application asset bundle.
  *
@@ -20,12 +21,20 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        //'css/site.css',
+        'https://use.fontawesome.com/releases/v5.2.0/css/all.css',
+        'https://fonts.googleapis.com/css?family=Varela+Round',
+        'css/hotel_style.css',
     ];
     public $js = [
+       //'js/scripts.js', 
+        'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js'
+    ];
+    public $jsOptions = [
+        'position' => \yii\web\View::POS_END
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset'
     ];
 }
