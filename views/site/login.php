4918eda3797fdfd4cabdeae730f90b419469abd5
Serega <ss.samsonov2035@yandex.ru>
2021-01-11 18:49:01 +0300

    Добавил кастомизацию формы. Порефакторил код

--- views/site/login.php
+++ views/site/login.php
@@ -2,37 +2,37 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-namespace  app\models\LoginForm; 
+/*namespace  app\models\LoginForm;*/ 
 
 use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
+use yii\widgets\ActiveForm;
 
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Войти';
 ?>
  	
 <main class="main_form-styles">
 		<div class="main__inner">
 			<div class="c-form-block main__form-block">
-				<?php $form = ActiveForm::begin(['id' => 'login-form','errorCssClass'=>'c-form__validError','options'=>['class'=>'c-form']])?>
-				
-					<div class="c-from__group">
-						<?=$form->field($model,'username')->textInput(['class'=>'c-form__input'])->input('username', ['placeholder' => "Ваше Имя", 'class'=>'c-form__input'])->label(false);?>						
-					</div>
+				<?php $form = ActiveForm::begin([
+					'id' => 'login-form',
+					'options'=>['class'=>'c-form'],
+					'fieldConfig' => [
+						'template' => '<div class="c-form__group">{input}</div>',
+					]
+				])?>
 					
+				<?=$form->field($model,'username')->textInput(['placeholder' => 'имя', 'class'=>'c-form__input']);?>						
+						
+				<?= $form->field($model,'password')->passwordInput(['placeholder' => 'пароль', 'class'=>'c-form__input']);?>
+
+				<?= $form->field($model,'rememberMe')->checkbox()->label('Запомнить меня',['class'=>'c-form__label']);?>
 					
-					<div class="c-from__group">
-						<?= $form->field($model,'password')->passwordInput(['class'=>'c-form__input'])->input('password', ['placeholder' => "пароль", 'class'=>'c-form__input'])->label(false);?>
-					</div>
-					<div class="c-from__group">
-						<?= $form->field($model,'rememberMe')->checkbox()->label('Запомнить меня',['class'=>'c-form__label']);?>
-					</div>
-					
-					<div class="c-from__group c-from__group_btn-block">
-						<?= Html::submitButton('Войти',['class'=>'btn c-from__btn'])?>
-					</div>	
+				<div class="c-form__group c-form__group_btn-block">
+					<?= Html::submitButton('Войти', ['class'=>'btn c-form__btn'])?>
+				</div>	
 				
 				<?php $form = ActiveForm::end()?>
+				
 			</div>	
 		</div>
 	</main>
