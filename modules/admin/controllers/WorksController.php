31a6fe773ff3070adc19e1876a5f5e68574c7978
Maxon <chebola.maxim@gmail.com>
2021-01-11 20:03:47 +0300

    Revert "Добавил методы для добавления, обновления, удаления и просмотра данных таблицы бронирования"
    This reverts commit c9a16ceb9c698c7c2b8da800906006bf7fe2960f.

--- modules/admin/controllers/WorksController.php
+++ modules/admin/controllers/WorksController.php
@@ -4,11 +4,6 @@ namespace app\modules\admin\controllers;
 
 use yii\web\Controller;
 use app\modules\admin\models\room;
-use app\modules\admin\models\bookSearch;
-use Yii;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
-use app\modules\admin\models\book;
 
 /**
  * Default controller for the `admin` module
@@ -38,59 +33,5 @@ class WorksController extends Controller
         $sql2 = 'SELECT book_room.IdBookRoom, client_hotel.Surname, client_hotel.Name, client_hotel.Partonymic, book_room.ArrivalDate, book_room.DepartDate, book_room.StatusCode, book_room.GroupSize From book_room LEFT JOIN client_hotel ON book_room.IdMainClient =  client_hotel.IdClient Order By book_room.IdBookRoom';
         $Hotbooks = room::findBySql($sql2)->asArray()->all();
         return $this->render('books',compact('Hotbooks'));
-    }
-    public function actionBook()
-    {
-        $searchModel = new bookSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-        
-        return $this->render('booktable', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
-    public function actionCreate()
-    {
-        $model = new book();
-        
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->IdBookRoom]);
-        }
-        
-        return $this->render('bookcreate', [
-            'model' => $model,
-        ]);
-    }
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-        
-        return $this->redirect(['index']);
-    }
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-        
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->IdBookRoom]);
-        }
-        
-        return $this->render('bookupdate', [
-            'model' => $model,
-        ]);
-    }
-    protected function findModel($id)
-    {
-        if (($model = book::findOne($id)) !== null) {
-            return $model;
-        }
-        
-        throw new NotFoundHttpException('The requested page does not exist.');
     }
 }
--- modules/admin/models/book.php
+++ modules/admin/models/book.php
@@ -3,44 +3,9 @@ namespace app\modules\admin\models;
 
 use yii\db\ActiveRecord;
 
-class book extends  ActiveRecord
+class room extends  ActiveRecord
 {
        public static function tableName() {// если отличается название таблицы
             return 'book_room' ;
-         }
-         public function getClient()
-         {
-             return $this->hasOne(room::className(), ['id' => 'IdClient']);
-         }
-         public function rules()
-         {
-             return [
-                 [['ArrivalDate','DepartDate','StatusCode'], 'required'],
-                 ['ArrivalDate', 'date', 'format' => 'php:Y-m-d'],
-                 ['DepartDate', 'date', 'format' => 'php:Y-m-d'],
-                 [['StatusCode'], 'string', 'max' => 32],
-                 [['IdMainClient'], 'integer'],
-                 [['Room'], 'integer'],
-                 [['GroupSize'], 'integer'],
-                 [['DepartDate'], 'date', 'format' => 'php:Y-m-d'],
-               
-             ];
-         }
-         
-         /**
-          * {@inheritdoc}
-          */
-         public function attributeLabels()
-         {
-             return [
-                 'IdBookRoom' => 'ID',
-                 'ArrivalDate' => 'Arrival',
-                 'DepartDate' => 'Depart',
-                 'StatusCode' => 'Status',
-                 'IdMainClient' => 'IdClient',
-                 'Room' => 'Взрослых',// тип комнаты
-                 'GroupSize' => 'Размер группы',// тип комнаты
-                 'DateCreate' => 'Создан',
-             ];
          }
 }
