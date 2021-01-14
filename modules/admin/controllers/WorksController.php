422310108a4d787f58f5eedb648359b87e40bd13
Serega <ss.samsonov2035@yandex.ru>
2021-01-14 03:48:01 +0300

    Подредактировал контроллер админки - упростил запрос получения списка номеров.

--- modules/admin/controllers/WorksController.php
+++ modules/admin/controllers/WorksController.php
@@ -15,22 +15,20 @@ use app\models\Book;
  */
 class WorksController extends Controller
 {
-    /**
-     * Renders the index view for the module
-     * @return string
-     */
-    public $layout = 'admin';
+     
     public function actionIndex()
     {
-        return $this->render('index');
+       return $this->actionRooms();
     }
     
     public function actionRooms()
     {
         
-        $sql2 = 'SELECT room.IdRoom, room_category.Name,room_category.GuestNumber,room_category.Cost, room_category.Info From room, room_category WHERE room.IdCategory = room_category.IdCategory Order By room.IdRoom';
-        $Hotrooms = Room::findBySql($sql2)->asArray()->all();
-        return $this->render('rooms',compact('Hotrooms'));
+ /*       $sql2 = 'SELECT room.IdRoom, room_category.Name,room_category.GuestNumber,room_category.Cost, room_category.Info From room, room_category WHERE room.IdCategory = room_category.IdCategory Order By room.IdRoom'; */
+   //     $Hotrooms = Room::findBySql($sql2)->asArray()->all();
+        $rooms = Room::find()->asArray()->all();
+
+        return $this->render('rooms', compact('rooms'));
     }
     public function actionBooks()
     {
