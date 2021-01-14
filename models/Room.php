3bd638bd41a6958e908c9cdc8ed1546c7dd637a8
Serega <ss.samsonov2035@yandex.ru>
2021-01-14 03:31:15 +0300

    Убрал ассоциацию с названием таблицы

--- models/Room.php
+++ models/Room.php
@@ -3,9 +3,7 @@ namespace app\models;
 
 use yii\db\ActiveRecord;
 
-class Room extends  ActiveRecord
+class Room extends ActiveRecord
 {
-    public static function tableName() {// если отличается название таблицы
-        return 'client_hotel' ;
-    }
+ 
 }
