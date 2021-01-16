518d781f3bde518778a72f2ec3afc14d30ef3ac7
Serega <ss.samsonov2035@yandex.ru>
2021-01-16 23:47:38 +0300

    Поправил лого ссылку в админке

--- modules/admin/views/layouts/admin.php
+++ modules/admin/views/layouts/admin.php
@@ -23,10 +23,8 @@ AdminAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
     <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
-            RS-DC Hotel
-        </a>
-
+        <?= Html::a('RS-DC Hotel', ['works/'], ['class' => 'navbar-brand col-md-3 col-lg-2 me-0 px-3']) ?>
+        
         <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
